c30ec99d9d487cc17b83f50913fde106ef287860
Jay Asole <jasole1111@gmail.com>
2022-07-10 12:13:19 +0530

    Created a static express app which can host our static pages for our UI developement

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 8000
+
+//Static path
+const staticPath = path.join(__dirname , '../public')
+
+// Built in Middleware
+app.use(express.static(staticPath))
+
+
+//Setting template engine for the project
+app.set("view engine", "hbs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(8000, ()=>{
+    console.log(`Listening to the port ${port}`)
+})
