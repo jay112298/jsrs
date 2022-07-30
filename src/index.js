eba98c734511e31045321e354e44905a0ae1ab60
Jay Asole <jasole1111@gmail.com>
2022-07-30 22:45:29 +0530

    Deploy ready

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 8000
+const port = process.env.PORT || 8000;
 
 //Static path
 const staticPath = path.join(__dirname , '../public')
@@ -11,7 +11,7 @@ app.use(express.static(staticPath))
 
 
 //Setting template engine for the project
-app.set("view engine", "hbs")
+// app.set("view engine", "hbs")
 
 app.get("/", (req, res) => {
     res.render("index")
